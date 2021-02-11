e4e7b5735877c500da0a2d8408265f9ffa9adcec
Daniel.Pollard <daniel.pollard@learnweaver.com>
2021-02-11 20:51:29 +1000

    First Init

---
+++ README.md
@@ -0,0 +1,35 @@
+# pxt-BBS-game-controller
+
+Custom blocks for BBS Controller for micro:bit. 
+See website for example code.
+
+## Buttons
+There are two different ways to trigger events when pressing the :GAME Controller buttons.
+
+One uses blocks which return 'True' or 'False' values - these can be inserted in 'if else' statements as shown below:
+
+```blocks
+if (BBS.buttonIsPressed(Kitronik_Game_Controller.ControllerButtonPins.Fire1)) {
+    <<Event 1>>
+} else if (BBS.buttonIsPressed(Kitronik_Game_Controller.ControllerButtonPins.Fire2)) {
+    <<Event 2>>
+}
+```
+
+The other is a stand-alone block which triggers an event when the condition is met:
+
+```blocks
+BBS.onButtonPress(BBS.ControllerButtonPins.Left, BBS.ControllerButtonEvents.Down, function () {
+    <<Event 3>>
+})
+```
+
+
+## Supported targets
+
+* for PXT/microbit
+(The metadata above is needed for package search.)
+
+## License
+
+MIT
---
+++ controller.ts
@@ -0,0 +1,83 @@
+
+//% weight=100 color=#00A654 icon="\uf11b" block=":GAME Controller"
+//% groups='["Inputs", "Feedback"]'
+namespace BBS_Game_Controller {
+	/**
+	*:GAME Controller Standard Buttons
+	*/
+	export enum ControllerButtons {
+	    Up,
+	    Down,
+	    Left,
+	    Right,
+	    Fire1,
+	    Fire2
+	}
+
+    /**
+    *:GAME Controller Button Pins
+    */
+    export enum ControllerButtonPins {
+        //% block="Joypad Up (P8)"
+        Up = DAL.MICROBIT_ID_IO_P16,
+        //% block="Joypad Down (P14)"
+        Down = DAL.MICROBIT_ID_IO_P13,
+        //% block="Joypad Left (P12)"
+        Left = DAL.MICROBIT_ID_IO_P8,
+        //% block="Joypad Right (P13)"
+        Right = DAL.MICROBIT_ID_IO_P12,
+        //% block="Fire 1 (P15)"
+        Fire1 = DAL.MICROBIT_ID_IO_P5,
+        //% block="Fire 2 (P16)"
+        Fire2 = DAL.MICROBIT_ID_IO_P11
+    }
+
+    /**
+    *:GAME Controller Button Events
+    */
+    export enum ControllerButtonEvents {
+        //% block="down"
+        Down = DAL.MICROBIT_BUTTON_EVT_DOWN,
+        //% block="up"
+        Up = DAL.MICROBIT_BUTTON_EVT_UP,
+        //% block="click"
+        Click = DAL.MICROBIT_BUTTON_EVT_CLICK
+    }
+
+    /**
+     *
+     */
+    //% shim=Kitronik_Game_Controller::init
+    function init(): void {
+        return;
+    }
+
+
+    /**
+     * Determines if a :GAME Controller button is pressed
+     * @param button press to be checked
+     */
+    //% group=Inputs
+    //% blockId="kitronik_controller_ispressed" block="button %button|is pressed"
+    //% button.fieldEditor="gridpicker" button.fieldOptions.columns=3
+    //% weight=95 blockGap=8
+    export function buttonIsPressed(button: ControllerButtonPins): boolean {
+        const pin = <DigitalPin><number>button;
+        pins.setPull(pin, PinPullMode.PullUp);
+        return pins.digitalReadPin(pin) == 0;
+    }
+
+    /**
+     * Do something when one of the :GAME Controller Buttons is pressed
+     * @param button press to be checked
+     * @param event happening on the button, eg: click
+     */
+    //% group=Inputs
+    //% blockId="kitronik_controller_button_press_on_event" block="on button %button|press %event"
+    //% button.fieldEditor="gridpicker" button.fieldOptions.columns=3
+    //% weight=93 blockGap=8
+    export function onButtonPress(button: ControllerButtonPins, event: ControllerButtonEvents, handler: Action) {
+        init();
+        control.onEvent(<number>button, <number>event, handler);
+    }
+} 
